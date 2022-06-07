1563b6ac27d1e47b7d2e3ef75da13b433797ede5
joaovhmota <joaovhmota2005@gmail.com>
2022-06-07 11:22:58 -0300

    Exercício 05

---
+++ C#/01 - OOP/AtividadesOOP/Exercício 05/CeaserCypher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercício_05
+{
+	internal class CeaserCypher
+	{
+		private string _InputString = "";
+		private string _DecrypedString = "";
+		private string _EncriptedString = "";
+		
+		public CeaserCypher Update(string str)
+		{
+			_InputString = str.ToLower();
+			return this;
+		}
+
+		public string Encrypt(int key = 1)
+		{
+			foreach(Char c in _InputString)
+			{
+				int ascii = c;
+				if (c + key <= 122)
+					_EncriptedString += (char)(ascii + key);
+				else
+					_EncriptedString += (char)(ascii % key) + 96;
+			}
+			return _EncriptedString;
+		}
+
+		public string Decrypt(int key = 1)
+		{
+			foreach(Char c in _InputString)
+			{
+				int ascii = c;
+				if (c - key >= 97)
+				{
+					_DecrypedString += (char) (ascii - key);
+				}
+				else
+					_DecrypedString += (char)(ascii + 26 - key);
+			}
+			return _DecrypedString;
+		}
+	}
+}
---
+++ C#/01 - OOP/AtividadesOOP/Exercício 05/Program.cs
@@ -0,0 +1,33 @@
+﻿using Exercício_05;
+
+CeaserCypher cypher = new CeaserCypher();
+
+string str = "",
+	  opt = "";
+
+int key = 0;
+
+do
+{
+	try
+	{
+		Console.Clear();
+		Console.WriteLine(" ・ 1 - Criptografar");
+		Console.WriteLine(" ・ 2 - Descriptografar");
+		Console.WriteLine(" ・ 3 - Finalizar");
+		opt = Console.ReadLine()!;
+		if (opt == "3") break;
+
+		Console.Write(" ・ Digite uma frase para criptografar: "); str = Console.ReadLine()!;
+		if (str == "") continue;
+
+		Console.Write(" ・ Digite uma chave para criptografia: "); key = Convert.ToInt32(Console.ReadLine()!);
+
+		switch (opt)
+		{
+			case "1": Console.WriteLine($" ・ Resultado: {cypher.Update(str).Encrypt(key)}"); break;
+			case "2": Console.WriteLine($" ・ Resultado: {cypher.Update(str).Decrypt(key)}"); break;
+		}
+	}
+	catch (Exception ex) { continue; }
+} while (true);
