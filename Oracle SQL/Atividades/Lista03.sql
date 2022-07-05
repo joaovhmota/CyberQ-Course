0dd70eb404537e92efb9a9f6023b122be3fc3bc4
joaovhmota <joaovhmota2005@gmail.com>
2022-07-05 17:16:57 -0300

    Backup 05/07/2022

--- Oracle SQL/Atividades/Lista03.sql
+++ Oracle SQL/Atividades/Lista03.sql
@@ -1,4 +1,4 @@
-CREATE TABLE alunos (
+﻿CREATE TABLE alunos (
 	nome VARCHAR2(40),
 	turma VARCHAR2(20),
 	nota1 NUMBER,
@@ -17,6 +17,29 @@ INSERT INTO alunos VALUES ('Luciana Biltz', '8B', 9.5, 10, 10, 'Vespertino');
 INSERT INTO alunos VALUES ('Paulo Almeida', '8A', 10, 10, 10, 'Matutino');
 INSERT INTO alunos VALUES ('Carla Malta', '7B', 3, 7, 9, 'Matutino');
 INSERT INTO alunos VALUES ('Diogo Andrade', '8C', 10, 9, 10, 'Vespertino');
-INSERT INTO alunos VALUES ('Mônica Mota', '8A', 7, 7, 8, 'Vespertino');
+INSERT INTO alunos VALUES ('M�nica Mota', '8A', 7, 7, 8, 'Vespertino');
 INSERT INTO alunos VALUES ('Juliana Lima', '8C', 9, 8, 2, 'Vespertino');
 INSERT INTO alunos VALUES ('Marcelo Matos', '7A', 9, 10, 5, 'Matutino');
+
+SELECT * FROM alunos;
+
+SELECT	nome, 
+		((nota1 + nota2 + nota3) / 3) as media ,
+		(CASE WHEN ((nota1 + nota2 + nota3) / 3) >= 7 THEN 'Aprovado' ELSE 'Reprovado' END) as situação
+FROM alunos;
+
+SELECT COUNT(*), turno FROM alunos GROUP BY turno;
+
+SELECT COUNT(*), turma FROM alunos GROUP BY turma;
+
+SELECT nome, ((nota1 + nota2 + nota3) / 3) as media FROM aluno WHERE ((nota1 + nota2 + nota3) / 3) >= 7;
+
+SELECT COUNT(*) FROM alunos WHERE ((nota1 + nota2 + nota3) / 3) < 7 AND turno = 'Vespertino';
+
+SELECT * FROM alunos WHERE ((nota1 + nota2 + nota3) / 3) = (SELECT MAX(((nota1 + nota2 + nota3) / 3)) FROM alunos);
+
+SELECT AVG((nota1 + nota2 + nota3) / 3) as media, turma FROM alunos GROUP BY turma;
+
+UPDATE alunos SET turma = '8A' WHERE turma = '8B';
+
+DROP TABLE alunos;
